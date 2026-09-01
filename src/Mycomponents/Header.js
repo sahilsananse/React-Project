c59214630874f25fd59cbd1f90f68e3b80de43ad
sahil sananse <sahilsananse@gmail.com>
2026-09-01 17:48:00 +0530

    add title

--- src/Mycomponents/Header.js
+++ src/Mycomponents/Header.js
@@ -7,7 +7,6 @@ export default function Header(props) {
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-
           <Link className="navbar-brand" to="/">
             {props.title}
           </Link>
@@ -24,30 +23,19 @@ export default function Header(props) {
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div
-            className="collapse navbar-collapse"
-            id="navbarSupportedContent"
-          >
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-
               <li className="nav-item">
-                <Link
-                  className="nav-link active"
-                  to="/"
-                >
+                <Link className="nav-link active" to="/">
                   Home
                 </Link>
               </li>
 
               <li className="nav-item">
-                <Link
-                  className="nav-link"
-                  to="/about"
-                >
+                <Link className="nav-link" to="/about">
                   About
                 </Link>
               </li>
-
             </ul>
 
             {props.searchbar && (
@@ -59,15 +47,11 @@ export default function Header(props) {
                   aria-label="Search"
                 />
 
-                <button
-                  className="btn btn-outline-success"
-                  type="submit"
-                >
+                <button className="btn btn-outline-success" type="submit">
                   Search
                 </button>
               </form>
             )}
-
           </div>
         </div>
       </nav>
